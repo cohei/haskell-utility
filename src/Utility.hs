eae560fa9dc15ffae2e06e09d7aeb9652039aaeb
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2019-12-22 15:43:36 +0900

    Add more test for `combinations`

--- src/Utility.hs
+++ src/Utility.hs
@@ -130,6 +130,8 @@ sequentialGroupBy p = foldr f []
 --
 -- >>> combinations 2 [1..4]
 -- [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+-- >>> combinations 3 [1,2,3,4]
+-- [[1,2,3],[1,2,4],[1,3,4],[2,3,4]]
 combinations :: Int -> [a] -> [[a]]
 combinations 0 _      = [[]]
 combinations _ []     = []
