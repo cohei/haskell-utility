6308f8853c1919e357d6c656e7cb122069460001
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2025-03-18 02:49:19 +0900

    Format

--- src/Utility.hs
+++ src/Utility.hs
@@ -1,29 +1,52 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-module Utility
-    (
-    -- * Combinators
-      y, owl
-    -- * for numbers
-    , toDigits, fromDigits, primeFactors, primes, factors
-    -- * for Alternative
-    , whenA, ensure
-    -- * for lists
-    , chop, allSame, diff, lastn, removeSharedPrefix, odds, evens, sequentialGroupBy
-    , combinations, permutations, partitions, foci, middle
-    -- * for tuples
-    , mapTuple
-    -- * for boolean
-    , implies
-    ) where
 
-import           Control.Applicative (Alternative (empty))
-import           Control.Arrow       ((***))
-import           Control.Monad       (guard, join)
-import           Data.Bifunctor      (second)
-import           Data.Bool           (bool)
-import           Data.List           (group, inits, unfoldr)
-import           Data.Tuple          (swap)
-import           Unsafe.Coerce       (unsafeCoerce)
+module Utility
+  ( -- * Combinators
+    y,
+    owl,
+
+    -- * for numbers
+    toDigits,
+    fromDigits,
+    primeFactors,
+    primes,
+    factors,
+
+    -- * for Alternative
+    whenA,
+    ensure,
+
+    -- * for lists
+    chop,
+    allSame,
+    diff,
+    lastn,
+    removeSharedPrefix,
+    odds,
+    evens,
+    sequentialGroupBy,
+    combinations,
+    permutations,
+    partitions,
+    foci,
+    middle,
+
+    -- * for tuples
+    mapTuple,
+
+    -- * for boolean
+    implies,
+  )
+where
+
+import Control.Applicative (Alternative (empty))
+import Control.Arrow ((***))
+import Control.Monad (guard, join)
+import Data.Bifunctor (second)
+import Data.Bool (bool)
+import Data.List (group, inits, unfoldr)
+import Data.Tuple (swap)
+import Unsafe.Coerce (unsafeCoerce)
 
 -- | Y combinator.
 --
@@ -42,7 +65,7 @@ y f = (\x -> f (unsafeCoerce x x)) (\x -> f (unsafeCoerce x x))
 
 -- | Combine an unary function and a binary function as @\\x y -> f (g x y) == owl f g@ .
 owl :: (c -> d) -> (a -> b -> c) -> a -> b -> d
-owl = (.).(.)
+owl = (.) . (.)
 
 -- | Get digits of an integer.
 --
@@ -50,7 +73,7 @@ owl = (.).(.)
 -- [1,2,3,4]
 -- >>> toDigits 2 42
 -- [1,0,1,0,1,0]
-toDigits :: forall a. Integral a => a -> a -> [a]
+toDigits :: forall a. (Integral a) => a -> a -> [a]
 toDigits base = reverse . unfoldr step
   where
     step :: (Monad m, Alternative m) => a -> m (a, a)
@@ -64,15 +87,15 @@ toDigits base = reverse . unfoldr step
 -- 1234
 -- >>> fromDigits 2 [1, 0, 1, 0, 1, 0]
 -- 42
-fromDigits :: Integral a => a -> [a] -> a
+fromDigits :: (Integral a) => a -> [a] -> a
 fromDigits base = foldl ((+) . (base *)) 0
 
 -- | Capsulate value in `Alternative`.
-whenA :: Alternative f => a -> Bool -> f a
+whenA :: (Alternative f) => a -> Bool -> f a
 whenA = bool empty . pure
 
 -- | `whenA` with a predicate.
-ensure :: Alternative m => (a -> Bool) -> a -> m a
+ensure :: (Alternative m) => (a -> Bool) -> a -> m a
 ensure = (whenA <*>)
 
 -- | Chop a list to lists of every @n@ elements.
@@ -80,35 +103,35 @@ chop :: Int -> [a] -> [[a]]
 chop n = unfoldr $ fmap (splitAt n) . ensure (not . null)
 
 -- | Return @True@ if the list has all the same elements.
-allSame :: Eq a => [a] -> Bool
-allSame []     = True
-allSame (x:xs) = all (x==) xs
+allSame :: (Eq a) => [a] -> Bool
+allSame [] = True
+allSame (x : xs) = all (x ==) xs
 
 -- | Compute implication.
 implies :: Bool -> Bool -> Bool
 implies True False = False
-implies _    _     = True
+implies _ _ = True
 
 -- | Get rear part of the longer list.
 diff :: [a] -> [a] -> [a]
-diff [] ys         = ys
-diff xs []         = xs
-diff (_:xs) (_:ys) = diff xs ys
+diff [] ys = ys
+diff xs [] = xs
+diff (_ : xs) (_ : ys) = diff xs ys
 
 -- | Get last @n@ elements of the list.
 lastn :: Int -> [a] -> [a]
---lastn n xs = diff xs $ drop n xs
+-- lastn n xs = diff xs $ drop n xs
 lastn = (diff <*>) . drop
 
 -- | Apply f on both tuple elements.
 --
 -- >>> mapTuple (+ 1) (3, 4)
 -- (4,5)
-mapTuple :: (a -> b) -> (a,a) -> (b,b)
+mapTuple :: (a -> b) -> (a, a) -> (b, b)
 mapTuple = join (***)
 
 -- | Remove shared prefix from two lists.
-removeSharedPrefix :: Eq a => [a] -> [a] -> ([a], [a])
+removeSharedPrefix :: (Eq a) => [a] -> [a] -> ([a], [a])
 removeSharedPrefix = (unzip .) . (dropWhile (uncurry (==)) .) . zip
 
 -- | Return elements in odd positions.
@@ -116,16 +139,16 @@ removeSharedPrefix = (unzip .) . (dropWhile (uncurry (==)) .) . zip
 -- >>> odds [1..10]
 -- [1,3,5,7,9]
 odds :: [a] -> [a]
-odds []     = []
-odds (x:xs) = x : evens xs
+odds [] = []
+odds (x : xs) = x : evens xs
 
 -- | Return elements in even positions.
 --
 -- >>> evens [1..10]
 -- [2,4,6,8,10]
 evens :: [a] -> [a]
-evens []     = []
-evens (_:xs) = odds xs
+evens [] = []
+evens (_ : xs) = odds xs
 
 -- | Group consequent series.
 --
@@ -135,8 +158,8 @@ sequentialGroupBy :: forall a. (a -> a -> Bool) -> [a] -> [[a]]
 sequentialGroupBy p = foldr f []
   where
     f :: a -> [[a]] -> [[a]]
-    f x (ys@(y':_):yss) | p x y' = (x:ys) : yss
-    f x yss             = [x]    : yss
+    f x (ys@(y' : _) : yss) | p x y' = (x : ys) : yss
+    f x yss = [x] : yss
 
 -- | List combinations of provided elements
 --
@@ -145,23 +168,23 @@ sequentialGroupBy p = foldr f []
 -- >>> combinations 3 [1,2,3,4]
 -- [[1,2,3],[1,2,4],[1,3,4],[2,3,4]]
 combinations :: Int -> [a] -> [[a]]
-combinations 0 _      = [[]]
-combinations _ []     = []
-combinations n (x:xs) = map (x:) (combinations (n - 1) xs) ++ combinations n xs
+combinations 0 _ = [[]]
+combinations _ [] = []
+combinations n (x : xs) = map (x :) (combinations (n - 1) xs) ++ combinations n xs
 
 -- | List permutations of provided elements
 --
 -- >>> permutations 2 [1..3]
 -- [[1,2],[1,3],[2,1],[2,3],[3,1],[3,2]]
 permutations :: Int -> [a] -> [[a]]
-permutations 0 _  = [[]]
+permutations 0 _ = [[]]
 permutations _ [] = [[]]
-permutations n xs = select xs >>= \(y', ys) -> map (y':) (permutations (n - 1) ys)
+permutations n xs = select xs >>= \(y', ys) -> map (y' :) (permutations (n - 1) ys)
 
 select :: [a] -> [(a, [a])]
-select []     = error "select: empty list"
-select [x]    = [(x, [])]
-select (x:xs) = (x, xs) : map (second (x:)) (select xs)
+select [] = error "select: empty list"
+select [x] = [(x, [])]
+select (x : xs) = (x, xs) : map (second (x :)) (select xs)
 
 -- | Partition of set by k
 --
@@ -180,8 +203,8 @@ partitions k = foldr f [replicate k []]
 -- >>> foci [1..3]
 -- [([],1,[2,3]),([1],2,[3]),([1,2],3,[])]
 foci :: [a] -> [([a], a, [a])]
-foci []     = []
-foci (x:xs) = ([], x, xs) : map (\(ys1, y', ys2) -> (x:ys1, y', ys2)) (foci xs)
+foci [] = []
+foci (x : xs) = ([], x, xs) : map (\(ys1, y', ys2) -> (x : ys1, y', ys2)) (foci xs)
 
 -- | Middle element of a list
 --
@@ -191,36 +214,36 @@ middle :: [a] -> a
 middle xs = middle' xs xs
 
 middle' :: [a] -> [a] -> a
-middle' []     _        = error "middle: empty list"
-middle' (m:_)  []       = m
-middle' (m:_)  [_]      = m
-middle' (_:ms) (_:_:xs) = middle' ms xs
+middle' [] _ = error "middle: empty list"
+middle' (m : _) [] = m
+middle' (m : _) [_] = m
+middle' (_ : ms) (_ : _ : xs) = middle' ms xs
 
 -- | List of prime numbers.
 --
 -- >>> take 10 primes
 -- [2,3,5,7,11,13,17,19,23,29]
-primes :: Integral a => [a]
+primes :: (Integral a) => [a]
 primes = 2 : filter (\n -> head (primeFactors n) == n) [3, 5 ..]
 
 -- | List prime factors.
 --
 -- >>> primeFactors 12312
 -- [2,2,2,3,3,3,3,19]
-primeFactors :: Integral a => a -> [a]
+primeFactors :: (Integral a) => a -> [a]
 primeFactors = primeFactors' primes
   where
-    primeFactors' :: Integral a => [a] -> a -> [a]
-    primeFactors' pps@(p:ps) n
-      | n < 2              = []
-      | n < p ^ (2 :: Int) = [n]  -- stop early
-      | n `mod` p == 0     = p : primeFactors' pps (n `div` p)
-      | otherwise          = primeFactors' ps n
+    primeFactors' :: (Integral a) => [a] -> a -> [a]
+    primeFactors' pps@(p : ps) n
+      | n < 2 = []
+      | n < p ^ (2 :: Int) = [n] -- stop early
+      | n `mod` p == 0 = p : primeFactors' pps (n `div` p)
+      | otherwise = primeFactors' ps n
     primeFactors' _ _ = error "`primes` is a infinite list"
 
 -- | List factors of a number.
 --
 -- >>> Data.List.sort $ factors 1232
 -- [1,2,4,7,8,11,14,16,22,28,44,56,77,88,112,154,176,308,616,1232]
-factors :: Integral a => a -> [a]
+factors :: (Integral a) => a -> [a]
 factors = map product . mapM (map product . inits) . group . primeFactors
